455d8f070da05f70e4c8b0ffac147bb682c0bdac
UosefHamza <uosefhamza0@gmail.com>
2024-09-15 17:58:09 +0300

    Part 01 Installing Bootstrap

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc33.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
