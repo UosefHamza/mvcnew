ea2c3eb4348e743b258118f780db40fabe58cc80
UosefHamza <uosefhamza0@gmail.com>
2024-09-15 17:08:25 +0300

    commit devv

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace mvc33
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Hello Worlddd!");
 
             app.Run();
         }
